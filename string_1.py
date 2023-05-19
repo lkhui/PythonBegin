7995ca8a8db8a7f07de9ce3c9528086047be01ee
lkhui <lkhui2000@hotmail.com>
2023-05-19 12:06:25 +0000

    another

--- string_1.py
+++ string_1.py
@@ -2,3 +2,12 @@ with open('string_1.txt', "r") as f:
     print(f.read())
 
 
+ss = ["Helloworld", "Testing me again", "ohno"]
+
+for s, n in zip(ss, [len(s) for s in ss]):
+    print(f'{s} - length {n}')
+
+arr = [(s, len(s)) for s in ss]
+
+for i in arr :
+    print(f'{i[0]} -- {i[1]}')
